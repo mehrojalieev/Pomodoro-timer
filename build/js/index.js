ea788579cd7e07465cf193bd560ce324735dc022
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-12-13 11:51:44 +0500

    Finished Count

--- build/js/index.js
+++ build/js/index.js
@@ -1,23 +1,30 @@
-// const ResultCount: HTMLElement = document.querySelector("#result-count")
-// const StartBtn: HTMLButtonElement = document.querySelector("#start-btn")
-// const PauseBtn: HTMLButtonElement = document.querySelector("#pause-btn")
-// const ResetBtn: HTMLButtonElement = document.querySelector("#reset-btn")
-// // function startTime(minute:number, second:number){
-// // }
-// StartBtn.addEventListener("click", () => {
-//         let minut: number = +ResultCount.innerText.split(":")[0]
-//         let second: number = +ResultCount.innerText.split(":")[1]
-//         while(minut > 0){
-//             if(second === 0){
-//                 minut -=1
-//                 second = 60
-//             }
-//             while(second > 0){
-//               const settime =  setTimeout(() => {
-//                     second -=1
-//                     console.log(`${minut}:${second}`);
-//                 }, 1000 )
-//                 clearTimeout(settime)
-//             }
-//         }
-// })
+const ResultCount = document.querySelector("#result-count");
+const StartBtn = document.querySelector("#start-btn");
+const PauseBtn = document.querySelector("#pause-btn");
+const ResetBtn = document.querySelector("#reset-btn");
+// function startTime(minute:number, second:number){
+// }
+let intervalId;
+StartBtn.addEventListener("click", () => {
+    let minut = +ResultCount.innerText.split(":")[0];
+    let second = +ResultCount.innerText.split(":")[1];
+    intervalId = setInterval(() => {
+        if (second === 0 && minut === 0) {
+            clearInterval(intervalId);
+            return;
+        }
+        if (second === 0) {
+            minut -= 1;
+            second = 60;
+        }
+        second -= 1;
+        ResultCount.innerText = `${minut} : ${second}`;
+    }, 1000);
+});
+ResetBtn.addEventListener("click", () => {
+    clearInterval(intervalId);
+    ResultCount.innerText = "25 : 00";
+});
+PauseBtn.addEventListener("click", () => {
+    clearInterval(intervalId);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,37 @@
-// const ResultCount: HTMLElement = document.querySelector("#result-count")
-// const StartBtn: HTMLButtonElement = document.querySelector("#start-btn")
-// const PauseBtn: HTMLButtonElement = document.querySelector("#pause-btn")
-// const ResetBtn: HTMLButtonElement = document.querySelector("#reset-btn")
+const ResultCount: HTMLElement = document.querySelector("#result-count")
+const StartBtn: HTMLButtonElement = document.querySelector("#start-btn")
+const PauseBtn: HTMLButtonElement = document.querySelector("#pause-btn")
+const ResetBtn: HTMLButtonElement = document.querySelector("#reset-btn")
 
 
-// // function startTime(minute:number, second:number){
+// function startTime(minute:number, second:number){
 
-// // }
+// }
 
-
-// StartBtn.addEventListener("click", () => {
-//         let minut: number = +ResultCount.innerText.split(":")[0]
-//         let second: number = +ResultCount.innerText.split(":")[1]
+let intervalId: number
+StartBtn.addEventListener("click", () => {
+        let minut: number = +ResultCount.innerText.split(":")[0]
+        let second: number = +ResultCount.innerText.split(":")[1]
     
-//         while(minut > 0){
-//             if(second === 0){
-//                 minut -=1
-//                 second = 60
-//             }
-//             while(second > 0){
-//               const settime =  setTimeout(() => {
-//                     second -=1
-//                     console.log(`${minut}:${second}`);
-                    
-//                 }, 1000 )
-//                 clearTimeout(settime)
-//             }
-//         }
+      intervalId =  setInterval(() => {
+            if(second === 0 && minut === 0){
+                clearInterval(intervalId)
+                return;
+            }
+            if(second === 0){
+                minut -=1 
+                second = 60
+            }
+            second -=1
+            ResultCount.innerText = `${minut} : ${second}`
+       }, 1000)
+})
 
-       
-// })
+ResetBtn.addEventListener("click", () => {
+    clearInterval(intervalId)
+    ResultCount.innerText = "25 : 00"
+})
+
+PauseBtn.addEventListener("click", () => {
+    clearInterval(intervalId)
+})
